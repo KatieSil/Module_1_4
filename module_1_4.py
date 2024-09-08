855c00b1b75552b75ceb026deefe12e5bfdd6b59
KatieSil <katiesil@mail.ru>
2024-09-08 17:57:36 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,7 @@
+my_string = input('Введите фразу или выражение: ')
+print('Количество символов введённого текста: ', len(my_string))
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(' ', ''))
+print('Первый символ строки: ', my_string[0])
+print('Последний символ строки: ', my_string[-1])
